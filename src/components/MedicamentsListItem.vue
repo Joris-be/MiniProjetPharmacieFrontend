ff3941abcbd4de696a2abeb06bad5e3dc6b4dcd0
Joris-be <joris.beldjoudi@gmail.com>
2026-03-03 10:17:11 +0100

    test commit

--- src/components/MedicamentsListItem.vue
+++ src/components/MedicamentsListItem.vue
@@ -1,12 +1,20 @@
 <script setup>
-defineProps(['med']); // reçoit le médicament
-defineEmits(['eventDelete']);
+defineProps(['med']);
+defineEmits(['eventDelete', 'eventIncrement', 'eventDecrement']);
 </script>
 
 <template>
   <li>
-    <button @click="$emit('eventDelete', med.id)">
-      Delete
-    </button>
+    {{ med.denomination }} - Stock: {{ med.qte }} - Forme:
+    {{ med.formepharmaceutique }}
+    <img
+      v-if="med.photo"
+      :src="'https://apipharmacie.pecatte.fr/images/' + med.photo"
+      alt="image du medicament"
+      width="150"
+    />
+    <button @click="$emit('eventIncrement', med)">+1</button>
+    <button @click="$emit('eventDecrement', med)">-1</button>
+    <button @click="$emit('eventDelete', med.id)">Delete</button>
   </li>
 </template>
---
+++ src/components/MedicamentsSearch.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue';
+const libelle = ref(''); // c’est une variable réactive
+const emit = defineEmits(['eventSearch']); // définir l'événement eventAdd
+</script>
+
+<template>
+  <h3>Recherche de Médicament :</h3>
+  <form @submit.prevent="$emit('eventSearch', libelle)">
+    <input v-model="libelle" type="text" placeholder="Nom de Médicament" />
+    <button type="submit">Rechercher</button>
+  </form>
+</template>
