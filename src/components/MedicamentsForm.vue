7e26f4f77c34551289328520db0833407fa6200c
Joris-be <joris.beldjoudi@gmail.com>
2026-03-04 10:29:03 +0100

    correction upload image

--- src/components/MedicamentsForm.vue
+++ src/components/MedicamentsForm.vue
@@ -3,9 +3,22 @@ import { ref } from 'vue';
 const denomination = ref(''); // le nom que l’utilisateur tape
 const formepharmaceutique = ref('Comprimé'); // valeur par défaut
 const qte = ref(1); // quantité par défaut
-const photo = ref(''); // URL ou nom de la photo
+const photo = ref(null); // --> variable qui va contenir le fichier en base64
 
 const emit = defineEmits(['eventAdd']);
+
+
+const handleFileUpload = (event) => {
+  const file = event.target.files[0];
+  if (!file) return; // Si l'utilisateur annule la sélection du fichier, on ne fait rien
+  // FileReader est un objet JavaScript permettant de lire le contenu d'un fichier 
+  // de manière asynchrone.
+  const reader = new FileReader(); 
+  reader.onload = () => { // definir le traitement asynchrone du contenu du fichier
+  photo.value = reader.result // --> convertit le contenu du fichier en base64
+  };
+  reader.readAsDataURL(file); // lance la lecture du fichier et donc la conversion en base64
+};
 
 function submitForm() {
   // Crée un objet médicament complet
@@ -20,7 +33,7 @@ function submitForm() {
   denomination.value = '';
   formepharmaceutique.value = '';
   qte.value = 1;
-  photo.value = '';
+  photo.value = null;
 }
 </script>
 
@@ -45,7 +58,7 @@ function submitForm() {
       min="0"
       required
     />
-    <input v-model="photo" type="text" placeholder="Nom ou URL de la photo" />
+    <input id="photo" @change="handleFileUpload" type="file" />
 
     <button type="submit">Ajouter</button>
   </form>
